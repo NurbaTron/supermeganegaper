991b3f1cb90065b8a31151b2579083fe622259df
EEEWWW <gulzatmm78@gmail.com>
2021-09-16 17:20:03 +0600

    Iloveniggers

---
+++ todo/todo/todoApp/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponseNotFound
+
+from .models import Todo
+
+# Получение данных из БД
+def homepage(request):
+    todos = Todo.objects.all()
+    return  render(request, "index.html", {'todos':todos})
+
+# Сохранение данных в БД
+def create(request):
+    if request.method == 'POST':
+        todo = Todo()
+        todo.title = request.POST.get('title')
+        todo.description = request.POST.get('description')
+        todo.save()
+    return HttpResponseRedirect('/')
+
+# Изменение данных из БД
+def edit(request, id):
+    try:
+        todo = Todo.objects.get(id=id)
+        if request.method == 'POST':
+            todo.title = request.POST.get("title")
+            todo.description = request.POST.get("description")
+            todo.save()
+            return HttpResponseRedirect('/')
+        
+        else:
+            return render(request, 'edit.html', {'todo':todo})
+    
+    except Todo.DoesNotExist:
+        return HttpResponseNotFound("<h2>Задачка не найдена</h2>")
+
+# Удаление данных из БД
+def delete(request, id):
+    try:
+        todo = Todo.objects.get(id=id)
+        todo.delete()
+        return HttpResponseRedirect('/')
+    except Todo.DoesNotExist:
+        return HttpResponseNotFound("<h2>Задачка не найдена</h2>")
