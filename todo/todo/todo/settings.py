5e6f48ae5de62d38825a6facb9ba1fd04297d59a
EEEWWW <gulzatmm78@gmail.com>
2021-09-17 15:11:47 +0600

    added file requirements.txt

--- todo/todo/todo/settings.py
+++ todo/todo/todo/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'todo',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'rem',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
